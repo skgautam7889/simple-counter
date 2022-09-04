1841a3d3cf05f881c1cd93583a33f4f554fae343
Suraj Kumar <surajkumargautam7889@gmail.com>
2022-09-04 16:39:33 +0530

    more changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import { React, useState } from 'react'
 
 function App() {
-  
+  const [counter, setCounter] = useState(0);
+
+  //increase counter
+  const increase = () => {
+    setCounter(count => count + 1);
+  };
+
+  //decrease counter
+  const decrease = () => {
+    setCounter(count => count - 1);
+  };
+
+  //reset counter 
+  const reset = () => {
+    setCounter(0)
+  }
   return (
-    <div className="app">
-      Hello Suraj Kumar
+    <div className="counter">
+      <h2>React Counter</h2>
+      <span className="counter__output">{counter}</span>
+      <div className="btn__container">
+        <button className="control__btn" onClick={increase}>+</button>
+        <button className="control__btn" onClick={decrease}>-</button>
+        <button className="reset" onClick={reset}>Reset</button>
+      </div>
     </div>
   );
 }
